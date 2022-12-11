03e72eac4a9951b9eb4aa0bb90832003cb4b7a84
Gabriel Frizanco <116903283+GabrielFrizanco@users.noreply.github.com>
2022-12-11 11:55:22 -0300

    Update QuantProd.sql

--- Views/QuantProd.sql
+++ Views/QuantProd.sql
@@ -1,16 +1,11 @@
---- CRIE UMA VIS√O INFORMANDO A QUANTIDADE DE PRODUTOS POR COD_PROD
-
+--- CRIE UMA VIS√ÉO INFORMANDO A QUANTIDADE DE PRODUTOS POR COD_PROD
 
 CREATE OR REPLACE VIEW VW_QUANT_PROD AS
-
 SELECT rp.cod_prod, rp.quant_prod, c.ds_cidade, c.uf
-
 FROM RegistroProdutos rp
-
 INNER JOIN Cidades c ON rp.id_cidade = c.id_cidade;
 
-
-
+--- EXECUTAR VIEW
 SELECT * FROM VW_QUANT_PROD
-
-WHERE cod_prod = 6;
+WHERE cod_prod = 6 AND
+uf = 'SP';
